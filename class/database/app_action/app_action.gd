99220948c6c3df083a697c9696f8361308ccc2be
Spiros Angelaki II <angelakis.Junior@gmail.com>
2025-10-26 18:33:46 -0500

    added MIT license and licensed all gd files

--- class/database/app_action/app_action.gd
+++ class/database/app_action/app_action.gd
@@ -1,3 +1,29 @@
+#|*******************************************************************
+# app_action.gd
+#*******************************************************************
+# This file is part of gd_app.
+# 
+# gd_app is an open-source software library.
+# gd_app is licensed under the MIT license.
+# https://github.com/gammasynth/gd_app
+#*******************************************************************
+# Copyright (c) 2025 AD - present; 1447 AH - present, Gammasynth.  
+# 
+# Gammasynth
+# 
+# Gammasynth (Gammasynth Software), Texas, U.S.A.
+# https://gammasynth.com
+# https://github.com/gammasynth
+# 
+# This software is licensed under the MIT license.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# 
+#|*******************************************************************
 extends Database
 
 ## AppAction class, override _make_undone and _make_redone in an extended class.
